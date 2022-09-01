0ca6e008fbebde30c3c2280e4b2ad07afd788ae3
Tiago Alves <tchaguwu@gmail.com>
2022-09-01 14:34:14 -0300

    fix: files organized and excess deleted

--- exercise2/js/buttons.js
+++ exercise2/js/buttons.js
@@ -1,19 +1,19 @@
 function handleBtn(id) {
-  let inputText = inputArea.value;
-  let lastChar = inputText.slice(-1);
-
   const element = document.getElementById(id);
   const text = element.textContent;
 
-  // allowed.test(element)
   if (op.includes(inputArea.value.slice(-1)) && op.includes(text)) {
     let value = inputArea.value.slice(0, -1);
     value += text;
     inputArea.value = value;
   } else if (id == "delete") {
-    inputText = inputArea.value.slice(0, -1);
-    inputArea.value = inputText;
+    handleDelete();
   } else {
     inputArea.value += text;
   }
+}
+
+function handleDelete() {
+  inputText = inputArea.value.slice(0, -1);
+  inputArea.value = inputText;
 }
--- exercise2/js/input.js
+++ exercise2/js/input.js
@@ -3,43 +3,23 @@ const inputArea = document.getElementById("input-area");
 const allowed = /[0-9+-.x*/]+/;
 const op = ["+", "-", "*", "x", "X", "/"];
 
-
-// Handler de tecla digitada no input
 function handleInput(e) {
-    // let inputText = inputArea.value;
-    // let lastChar = inputText.slice(-1);
-
-  // Se Backspace for pressionado
   if (e.data == null) {
-    function handleDelete() {
-        inputArea.value.slice(0, -1);
-    }
-    handleDelete();
-    // Ou se qualquer tecla proibida for pressionada
   } else if (!allowed.test(e.data)) {
-    // Não vai ser digitada
     e.preventDefault();
   }
 
-  // Se digitarem um operador duas vezes
-  function handleOp() {
-    if (op.includes(inputArea.value.slice(-1)) && op.includes(e.data)) {
-      // Deleta o digitado primeiro
-      let value = inputArea.value.slice(0, -1);
-      inputArea.value = value;
-    }
+  if (op.includes(inputArea.value.slice(-1)) && op.includes(e.data)) {
+    let value = inputArea.value.slice(0, -1);
+    inputArea.value = value;
   }
-  handleOp();
 }
 
-// Handler de assim que a tecla for digitada
-inputArea.addEventListener("beforeinput", e => handleInput(e));
+inputArea.addEventListener("beforeinput", (e) => handleInput(e));
 
 function handleEquals(e) {
-    // console.log(eval(inputArea.value));
-    e.preventDefault();
-    inputArea.value = eval(inputArea.value);
-    return false;
+  e.preventDefault();
+  inputArea.value = eval(inputArea.value);
 }
 
-form.addEventListener("submit", e => handleEquals(e));
+form.addEventListener("submit", (e) => handleEquals(e));
