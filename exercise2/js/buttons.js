68d34e3afc2d0f4718a800997a15de6adb5c3703
Tiago Alves <tchaguwu@gmail.com>
2022-09-01 14:25:37 -0300

    fix: placing one operator after the other via btns

--- exercise2/js/buttons.js
+++ exercise2/js/buttons.js
@@ -1,20 +1,19 @@
 function handleBtn(id) {
-    let inputText = inputArea.value;
-    let lastChar = inputText.slice(-1);
-    
-    const element = document.getElementById(id);
-    const text = element.textContent;
+  let inputText = inputArea.value;
+  let lastChar = inputText.slice(-1);
 
-    // allowed.test(element)
-    if(false) {
-        // if (op.includes(inputText.slice(-1))) {
-        //     inputText += inputText.slice(-1);
-        //   }
-    } else if (id == 'delete') {
-        inputText = inputArea.value.slice(0, -1);
-        inputArea.value = inputText;
-    } else {
-        inputArea.value += text;
-    }
-    console.log(text);
+  const element = document.getElementById(id);
+  const text = element.textContent;
+
+  // allowed.test(element)
+  if (op.includes(inputArea.value.slice(-1)) && op.includes(text)) {
+    let value = inputArea.value.slice(0, -1);
+    value += text;
+    inputArea.value = value;
+  } else if (id == "delete") {
+    inputText = inputArea.value.slice(0, -1);
+    inputArea.value = inputText;
+  } else {
+    inputArea.value += text;
+  }
 }
