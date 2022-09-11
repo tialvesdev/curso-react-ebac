9e48b16edbd92700b5efce8ec8e7d73d3578c5ae
Tiago Alves <tchaguwu@gmail.com>
2022-09-10 22:41:02 -0300

    fix: table's scrollbar stylized

---
+++ exercise3/js/table.js
@@ -0,0 +1,32 @@
+
+// const people = {
+//     1: {
+//         name: "Jão Arvis",
+//         age: "Entre 18 e 30",
+//         pronouns: {
+//             1: "Ele/Dele",
+//             2: "Ela/Dela"
+//         },
+//         maritage: "Noivo"
+//     }
+// }
+
+// const people = [
+//     [
+//         "Jão Arvis",
+//         "Entre 10 e 30",
+//         [
+//             "Ela/Dela",
+//             "Ele/Dele"
+//         ],
+//         "Noivo"
+//     ],
+//     [
+//         "Diana Santos",
+//         "Mais de 60",
+//         ["Elu/Delu"],
+//         "Viúvo"
+//     ]
+// ]
+
+// console.log(people);
