09fc506e48563f22d6e1a52d0f7537f75d33f779
Tiago Alves <tchaguwu@gmail.com>
2022-09-26 16:00:21 -0300

    feat: getting maritage from form and values corrected in html

--- exercise3/js/form.js
+++ exercise3/js/form.js
@@ -1,14 +1,19 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const nameValue = document.getElementById('name').value;
-    console.log(nameValue);
+  const nameValue = document.getElementById("name").value;
+  console.log(nameValue);
 
-    const ageValue = document.querySelector('input[name="age"]:checked').value;
-    console.log(ageValue);
+  const ageValue = document.querySelector('input[name="age"]:checked').value;
+  console.log(ageValue);
 
-    const gendersValue = document.querySelector('input[name="age"]:checked').value;
-    console.log(ageValue);
+  // document.getElementById('anot').value = document.getElementById('anot-text').value;
+
+  // const gendersValue = document.querySelectorAll('input[type="checkbox"]:checked').value;
+  // console.log(gendersValue);
+
+  const maritageValue = document.getElementById("marital").value;
+  console.log(maritageValue)
 });
