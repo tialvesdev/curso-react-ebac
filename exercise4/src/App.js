0f0c630330dcf140ae715db7ee4c6019333b594f
Tiago Alves <tchaguwu@gmail.com>
2022-10-03 14:20:19 -0300

    feat: bg and date component

--- exercise4/src/App.js
+++ exercise4/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DateTime from "./components/utils/dateTime/DateTime";
+import Text from "./components/utils/text/Text";
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="main">
+        <Text p="teste"/>
+        <DateTime/>
+      </div>
     </div>
   );
 }
---
+++ exercise4/src/components/helloWorld1/HelloWorld1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld1 = () => {
+    
+}
+
+export default HelloWorld1;
