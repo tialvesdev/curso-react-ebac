42b3329cc29dbbcb8b6409d7f379928ea1e41bb7
ti-alves <tchaguwu@gmail.com>
2022-12-06 11:24:51 -0300

    delete

--- exercise5/src/component/utils/subText/SubText.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './subText.style.css'
-
-// Component for a not the main text
-const SubText = (props) => {
-    return <span className="subText">{props.text}</span>
-}
-
-export default SubText
