e162c96073d37edecca9a3759709dd79f76589ea
ti-alves <tchaguwu@gmail.com>
2022-12-07 14:39:20 -0300

    feat: keydown event checking allowed char and backspace

--- exercise5/src/components/viewfinder/Viewfinder.jsx
+++ exercise5/src/components/viewfinder/Viewfinder.jsx
@@ -3,10 +3,33 @@ import Subtext from "../utils/subtext/Subtext";
 import './viewfinder.style.css'
 
 class Viewfinder extends React.Component {
+
+    componentDidMount() {
+        let everything = "";
+
+        document.getElementById('display').addEventListener('keydown', (e) => {
+            const value = e.key;
+            const allowed = /[0-9+-.x*/]+/;
+
+            console.log(`value = ${value}`);
+            if (value === "Backspace") {
+                console.log("Backspace pressed");
+                everything.slice(0, -1)
+            } else if (!allowed.test(value)) {
+                e.preventDefault();
+                console.log("value is not allowed");
+            } else {
+                everything += value;
+            }
+            console.log(`everything = ${everything}`)
+
+        });
+    }
+
     render() {
         return (
             <div className="viewf">
-                <input type="text" className="input" />
+                <input id="display" type="text" className="input" />
                 <Subtext value="result" />
             </div>
         )
